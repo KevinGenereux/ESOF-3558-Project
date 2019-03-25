fbd0dd704faf485939162bbd2888e6b45dcd5fdc
Matthew Giorno <32398671+Negative-Feedback@users.noreply.github.com>
2019-03-25 18:36:07 -0400

    Added some comments

--- CompSimpsons.m
+++ CompSimpsons.m
@@ -1,14 +1,19 @@
 function I = CompSimpsons(a, b, n, f)
-h = (b-a)/2;
+%{
+    a - a value
+    b - b value
+    n - number of segments
+%}
+h = (b-a)/n;
 oddTotal = 0;
 evenTotal = 0;
-
+X = a:h:b
 for i = 1:2:n-1
-    oddTotal = f(h*i);
+    oddTotal = f(X(i));
 end
 
 for i = 2:2:n-2
-    evenTotal = f(h*i);
+    evenTotal = f(X(i));
 end
 oddTotal = oddTotal*4;
 evenTotal = evenTotal*2;
