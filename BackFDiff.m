e063a24337977357df2df63a06a8d90738dfd63f
Kevin Genereux <35746847+KevinGenereux@users.noreply.github.com>
2019-04-05 20:32:57 -0400

    Update BackFDiff.m

--- BackFDiff.m
+++ BackFDiff.m
@@ -1,8 +1,19 @@
-function D = BackFDiff(x, h, f)
 %{
+***Test Data***
+Input: BackFDiff(3, 0.5, @(x) x^3 + 3*x^2 + 5)
+Output: 39.2500
+Expected Output: 39.2500
+%}
+
+%{
+  ***Parameters*** 
   x - x value
   h - step value
   f - fucntion
 %}
+function D = BackFDiff(x, h, f)
+
+%calculating Backward Finite-Difference O(h)-first Derivative
 D = (f(x) - f(x-h))/h;
+
 end
