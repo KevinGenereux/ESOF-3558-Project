43687744933632b653c63e9ab7d431240a441537
Kevin Genereux <35746847+KevinGenereux@users.noreply.github.com>
2019-04-05 21:52:40 -0400

    Update CompSimpsons.m

--- CompSimpsons.m
+++ CompSimpsons.m
@@ -1,22 +1,36 @@
-function I = CompSimpsons(a, b, n, f)
 %{
-    a - a value
-    b - b value
-    n - number of segments
+    ***Test Case***
+    Input: CompSimpsons(0, 4, @(x) 1- 2.71828^(-x))
+    Output: 3.0158
+    Expected Output: 3.0158
+    Solved Integral: 3.018
 %}
+
+%{
+    ***Parameters***
+    a - lower bound of integral
+    b - upper bound of integral
+    f - function
+%}
+
+% estimate of integral using composite integral
+function I = CompSimpsons(a, b, f)
+
+% number of segments in Composite Simpsons is 5 by default
+n=5
+
+% calculating step size
 h = (b-a)/n;
-oddTotal = 0;
-evenTotal = 0;
+
+% creating a 
 X = a:h:b
-for i = 1:2:n-1
-    oddTotal = f(X(i));
-end
 
-for i = 2:2:n-2
-    evenTotal = f(X(i));
-end
-oddTotal = oddTotal*4;
-evenTotal = evenTotal*2;
+% calculating the integral of first two segments using Simpson's 1/3 Rule 
+TwoSegmentsIntegral = (h/3) * (f(X(1)) + 4*f(X(2)) + f(X(3)));
+% calculating the integral of last three segments using Simpson's 3/8 Rule 
+ThreeSegmentsIntegral = (3*h/8) * (f(X(3)) + 3*f(X(4)) + 3*f(X(5)) + f(X(6)));
 
-I = (b-a) * (f(a) + oddTotal + evenTotal + f(b))/(3*n);
+% sum or results for first two segments and last three segments
+integral = TwoSegmentsIntegral + ThreeSegmentsIntegral
+
 end
