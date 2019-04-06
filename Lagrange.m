d861548d3c45c03bee50b764ad6a02996e86e2d5
Kevin Genereux <35746847+KevinGenereux@users.noreply.github.com>
2019-04-05 22:54:52 -0400

    Update Lagrange.m

--- Lagrange.m
+++ Lagrange.m
@@ -1,22 +1,58 @@
-function fx = Lagrange(X, Y, x)
-%{
-   X - array of x values
-   Y - array of y values
-   x - value to calculate y at
+%{   
+    ***Test Data***
+    Input: Lagrange([0,1.8,5,6,8.2,9.2,12],[26,16.415,5.375,3.5,2.015,2.54,8],3.5)
+    Output: 9.5938
+    Expected Output: 9.5938
 %}
-n = length(X);
-L = zeros(n,1);
-for i = 1:n
-   temp = 1;
-    for j = 1:n
-        if j ~= i
-           temp = temp* ( (x - X(j))/(X(i) - X(j)) );
-        end
-    end
-    L(i) = temp;
+
+%{
+    ***Parameters***
+    x - array of independent variables
+    y - array of dependent variables
+    xx - the value of x you are trying to interpolating for y
+%}
+
+function yint = Lagrange(x,y,xx)
+% Lagrange: Lagrange interpolating polynomial
+% yint = Lagrange(x,y,xx): Uses an (n − 1) −order
+% Lagrange interpolating polynomial based on n data points
+% to determine a value of the dependent variable (yint) at
+% a given value of the independent variable, xx.
+% input:
+% x = independent variable
+% y = dependent variable
+% xx = value of independent variable at which the
+% interpolation is calculated
+% output:
+% yint = interpolated value of dependent variable
+
+% determine the max dimension length of a matrix
+n = length(x);
+
+% if the matrix is not n x n, display na error
+if length(y) ~= n
+    error('x and y must be same length');
 end
 
-fx = 0;
+% stores result
+s = 0;
+
+% iterating through each row of matrix
 for i = 1:n
-    fx = fx + L(i)*Y(i); 
+    
+    % initializing product variable to each element in y array
+        product = y(i);
+    for j = 1:n
+        
+        % calculating lagrange product
+        if i ~= j
+            product = product*(xx - x(j))/(x(i) - x(j));
+        end
+    end
+    
+    %summing individual lagrange products
+    s = s + product;
 end
+
+%interpolated value of y using lagrange polynomials
+yint = s;
